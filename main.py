07c9c9bf684ae87c5bf7f5b3daa7ed0d856f501d
WONSZ <mf.fliszkiewicz@gmail.com>
2021-11-16 01:33:53 +0100

    Second2

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ request = requests.get("https://api.viewbase.com/exchange").text
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+pd.set_option('display.float_format', '{:.4f}'.format)
+#pd.set_option('display.expand_frame_repr', False)
 
 data = pd.read_json(request)
 
